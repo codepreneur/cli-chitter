e99fa1b27ea0f3d4fc3ac0ebd1c19a3baaf26926
gomenasaai <contramundee@gmail.com>
2014-08-18 11:59:25 +0100

    added initial dependencies

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -2,4 +2,13 @@ require 'time'
 
 class Chitter
 
+	attr_reader :data
+
+	def initialize(writer,reader)
+		@writer = writer
+		@reader = reader
+		@data = {}
+		@exit = false
+	end
+
 end
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -2,4 +2,13 @@ require 'chitter'
 
 describe Chitter do
 
+	before(:each) do
+		@writer = double('writer')
+		@reader = double('reader')
+    @chitter = Chitter.new(@writer, @reader)
+    @time_now = Time.parse("Feb 24 2014") 
+    # 2014-02-24 00:00:00 +0000 
+  end
+
+
 end
