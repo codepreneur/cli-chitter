5f78d8df65e7c94872513be4fbc4c6666db1645e
gomenasaai <contramundee@gmail.com>
2014-08-18 12:07:49 +0100

    added time difference between post time and now

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -11,4 +11,12 @@ class Chitter
 		@exit = false
 	end
 
+	def time_difference(post_time)
+		diff = Time.now - post_time
+		if diff > 60
+			return ' (' + (diff/60).round.to_s + ' minute(s) ago)'
+		end
+		return ' (' + (diff).round.to_s + ' second(s) ago)'
+	end
+
 end
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -10,5 +10,16 @@ describe Chitter do
     # 2014-02-24 00:00:00 +0000 
   end
 
+  context "can calculate time difference in" do
+  	it "seconds" do
+  		expect(@chitter.time_difference(Time.now - 30)).to eq " (30 second(s) ago)" 
+  	end
+
+  	it "minutes" do
+			expect(@chitter.time_difference(Time.now - 120)).to eq " (2 minute(s) ago)"
+  	end
+  end
+
+  
 
 end
